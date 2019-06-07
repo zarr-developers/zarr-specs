670ef3689759a807e0af2df1891deb9e1072c5ad
Alistair Miles <alimanfoo@googlemail.com>
2019-06-07 17:49:15 +0200

    Core protocol v3.0 - illustrate data type extension (datetimes and timedeltas) (#35)
    * illustrative draft of a data type extension (datetimes)
    
    * suppress duplicate citation warning

--- docs/conf.py
+++ docs/conf.py
@@ -50,3 +50,8 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+suppress_warnings = [
+    # suppress "duplicate citation" warnings
+    'ref.citation',
+]
